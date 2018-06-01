b02b3602a80e57e7f62f7c7eeaf8cb700e4f2325
unknown <virruetamontalvo@hotmail.com>
2018-06-01 11:31:40 -0500

    cleaning hidden property

--- pages/login/account-locked.js
+++ pages/login/account-locked.js
@@ -78,7 +78,7 @@ class AccountLocked extends Component{
                             </p>
                             
                             <Embed style={{ marginBottom: 10 } }
-                                icon='hidden'
+                                icon=''
                                 id='6Gf_kRE4MJU'
                                 placeholder='https://farm1.staticflickr.com/830/28305627398_ae4c376da8_z.jpg'
                                 source='youtube'
