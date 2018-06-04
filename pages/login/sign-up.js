8142e00c5728f7bd989e0fac0cf9f26b8405a954
Moises Navarro <moises.navarro@meetliquid.com>
2018-06-04 11:16:58 -0500

    update files

--- pages/login/sign-up.js
+++ pages/login/sign-up.js
@@ -32,9 +32,9 @@ class SignUp extends Component{
       verticalAlign='middle'
     >
     
-      <Grid.Column style={{ maxWidth: 450 }}>
+      <Grid.Column style={{ maxWidth: 700 }}>
         <Header as='h2' color='teal' textAlign='center'>
-        You are almost done
+        <h1>You’re almost done</h1>
         </Header>
         <Form size='large'>
           <Segment stacked>
@@ -45,13 +45,13 @@ class SignUp extends Component{
               fluid
               icon='user'
               iconPosition='left'
-              placeholder='E-mail address'
+              placeholder='Email'
             />
             <Form.Input
               fluid
               icon='lock'
               iconPosition='left'
-              placeholder='Password'
+              placeholder='User name'
               type='password'
             />
 
