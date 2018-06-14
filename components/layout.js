fdea5f20725581b7f8b69b1d4b67f0815f779ac9
Moises Navarro <moises.navarro@meetliquid.com>
2018-06-14 14:29:48 -0500

    update header

--- components/layout.js
+++ components/layout.js
@@ -12,6 +12,7 @@ export default (props)=> {
 
         <Container>
             <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1"/>
                 <link rel="stylesheet" 
                 href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
                 <link rel="stylesheet" 
--- components/layout1.js
+++ components/layout1.js
@@ -10,6 +10,7 @@ export default (props)=> {
             <Container textAlign='center'>
        
             <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" class="next-head" />
                 <link rel="stylesheet" 
                 href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
                 <link rel="stylesheet" 
