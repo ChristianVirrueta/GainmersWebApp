e0f89f6d1b4ce4d6639fdee4d09d85e56a9b669c
unknown <julio16lb@gmail.com>
2018-06-20 14:12:43 -0500

    [backend] configuration account user

--- components/header.js
+++ components/header.js
@@ -1,21 +1,71 @@
-import React,{ReactElement} from 'react';
+import React,{ReactElement,Component} from 'react';
 import { Menu, Dropdown, Image, Icon,Button,Message } from 'semantic-ui-react';
 import{ Link,Routes} from '../routes';
 import web3 from '../ethereum/web3';
 
-export default ()=> {
-    let firstAcc= " ";
-    
-    web3.eth.getAccounts().then(e => {
-        firstAcc=e;
-            document.querySelector(".account")
-            .innerHTML=firstAcc;     
-    });
-    
-   
+const options = [
+    {
+      key: 1,
+      text: '',
+      value: 1,
+      content:  <span>
+                    <Icon name='cogs' />
+                    <Link route='/configuration'>
+                    <a> Configuration</a>
+                    </Link>
+                </span>,
+    },
+    //{
+      //key: 2,
+      //text: 'Configuration123',
+      //value: 2,
+      //content:  <span><Icon name='currency' /><Link route='/mybets'><a> My Bets</a></Link></span>,
+    //}
+];
 
-    return (
-        <Menu style={{marginTop: '40px', marginBottom: '60px'}}  secondary>
+let firstAcc= " ";
+class Header extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            address:''
+        };
+    };
+    api_call = async function(path, method){
+        const rest = await fetch(path,{
+            method: method
+        });
+        const response = await rest.json();
+        return response;
+    };
+    componentDidMount(){
+        let current = this;
+        let server_api = 'http://api.gainmers.io/api/users/find';
+        web3.eth.getAccounts().then(e => {
+            firstAcc=e;
+            //document.querySelector(".account").innerHTML=firstAcc;
+            current.setState({
+                address: firstAcc[0]
+            });
+
+            server_api = server_api+'?address='+firstAcc[0];
+            current.api_call(server_api, 'GET').then(function(response){
+                if(response.length === 1){
+                  current.setState({
+                    name: response[0].name,
+                    email: response[0].email
+                  });
+                }
+            });
+        });
+    }
+
+
+    render(){
+        return(
+            <Menu style={{marginTop: '40px', marginBottom: '60px'}}  secondary>
             <Link route="/betting">
                 <a className="item"><Image src='http://dapp.gainmers.io/assets/images/logo.png' size='small'/></a>
             </Link>
@@ -27,17 +77,18 @@ export default ()=> {
                     </Dropdown.Menu>
                     </Dropdown>
                 </Menu.Item>
-
                 <Menu.Item>
                     <span className="item">Wallet : </span>
-
-                    <div className="wallet account"/>
-  
+                    <div className="wallet account">{this.state.address}</div>
                 </Menu.Item>
-
-          
+                <Menu.Item>
+                    <Dropdown trigger={<span><Icon name='user' /> {this.state.name}</span>}  options={options} item >
+                    </Dropdown>
+                </Menu.Item>
             </Menu.Menu>
        </Menu>
-    );
-  
-}
+        );
+    }
+};
+
+export default Header;
